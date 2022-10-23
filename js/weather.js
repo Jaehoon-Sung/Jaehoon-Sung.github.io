a2f11fa133ad79a1dbc9951e5a33bb4555bad6d1
Jaehoon Sung <jaehoon4567@gmail.com>
2022-10-23 16:58:28 -0400

    weather finished

--- js/weather.js
+++ js/weather.js
@@ -1,11 +1,21 @@
-navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+const API_KEY = "64f1c61deb4fa6913642b8a92cf68a74";
 
-function onGEoOk(position) {
+function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lng = position.coords.longitude;
-  consolg.log(`${lat}, ${lng}`);
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const city = document.querySelector("#weather span:first-child");
+      const weather = document.querySelector("#weather span:last-child");
+      city.innerHTML = data.name;
+      weather.innerHTML = data.weather[0].main;
+    });
 }
 
 function onGeoError() {
   alert("No weather for you.");
 }
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
